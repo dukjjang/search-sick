99dc095d5ec216b69d4a8c6afe3b2aeb63ee7368
dukjjang <jhd7292@naver.com>
2023-01-11 01:29:39 +0900

    design: 반응형 css 추가

--- src/components/SearchManager/index.tsx
+++ src/components/SearchManager/index.tsx
@@ -1,19 +1,16 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { RxMagnifyingGlass } from 'react-icons/rx';
 
 export default function Search() {
-  const [editMode, setEditMode] = useState(false);
   const searchRef = useRef<HTMLInputElement>(null);
 
-  const handleEditMode = () => {
-    setEditMode(!editMode);
-  };
+  const handleEditMode = () => {};
 
   return (
     <div
       role='presentation'
       onClick={handleEditMode}
-      className='  md:flex relative w-full h-full md:w-[500px] bg-white md:rounded-3xl lg:text-lg '
+      className=' md:flex-col md:flex fixed md:focus-within:top-auto focus-within:top-0 md:focus-within:h-fit focus-within:w-full max-w-[500px] focus-within:h-full focus-within:rounded-none md:focus-within:rounded-3xl  md:w-[500px] bg-white rounded-3xl lg:text-lg '
     >
       <label className='flex w-full pl-3 border-b md:border-0' htmlFor='search'>
         <div className=' flex justify-center items-center'>
@@ -25,18 +22,19 @@ export default function Search() {
           className=' peer w-full p-3 lg:p-4 roundeh-lg '
           type='text'
         />
+
         <ul
-          className={` peer-focus:block hidden  absolute w-full z-10 top-[60px] left-0 lg:top-[70px] border rounded-lg bg-white overflow-hidden`}
+          className={` peer-focus:block hidden absolute w-full z-10 top-[60px] right-0 left-0 lg:top-[70px] border rounded-lg bg-white overflow-hidden`}
         >
           <li className='p-1'>결과없음</li>
           <li className='p-1 hover:bg-neutral-100 bg-neutral-50'>결과</li>
         </ul>
-        <button
-          className='w-24 bg-[#1976D2] text-white whitespace-nowrap md:rounded-r-3xl'
-          type='button'
+        <div
+          id='searchButton'
+          className='flex justify-center items-center w-24 bg-[#1976D2] text-white whitespace-nowrap peer-focus:rounded-none md:peer-focus:rounded-r-3xl rounded-r-3xl'
         >
           검색
-        </button>
+        </div>
       </label>
     </div>
   );
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,8 +2,8 @@ import Search from '../components/SearchManager';
 
 export default function Main() {
   return (
-    <div className='fixed w-full h-full md:pt-20 md:px-5 bg-[#CAE9FF]'>
-      <div className='hidden md:flex flex-col justify-center items-center w-full mb-5 gap-3 text-[1.25rem] lg:text-[2.125rem] font-bold'>
+    <div className=' fixed w-full h-full pt-20 md:px-5 bg-[#CAE9FF]'>
+      <div className=' flex flex-col justify-center items-center w-full mb-5 gap-3 text-[1.25rem] lg:text-[2.125rem] font-bold'>
         <h2 className='m-auto'>국내 모든 임상시험 검색하고</h2>
         <h2> 온라인으로 참여하기</h2>
       </div>
