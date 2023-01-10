5176e72b5cfe05bc0051734f6b36bca62cf7f78e
dukjjang <jhd7292@naver.com>
2023-01-11 00:06:24 +0900

    feat: edit mode state -> css input focus로 의존성 변경

--- src/components/SearchManager/index.tsx
+++ src/components/SearchManager/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { RxMagnifyingGlass } from 'react-icons/rx';
 
 export default function Search() {
   const [editMode, setEditMode] = useState(false);
+  const searchRef = useRef<HTMLInputElement>(null);
 
   const handleEditMode = () => {
     setEditMode(!editMode);
@@ -12,28 +13,31 @@ export default function Search() {
     <div
       role='presentation'
       onClick={handleEditMode}
-      className={` flex justify-center items-center `}
+      className='  md:flex relative w-full h-full md:w-[500px] bg-white md:rounded-3xl lg:text-lg '
     >
-      <div className='relative w-[500px] bg-white rounded-3xl lg:text-lg'>
-        <div className='peer flex justify-center pl-4 rounded-3xl overflow-hidden '>
-          <label className=' flex w-full' htmlFor='search'>
-            <div className=' flex justify-center items-center'>
-              <RxMagnifyingGlass size={24} opacity='0.7' />
-            </div>
-            <input id='search' className='w-full p-4 roundeh-lg ' type='text' />
-          </label>
-          <button
-            className='w-24 bg-[#1976D2] text-white whitespace-nowrap'
-            type='button'
-          >
-            검색
-          </button>
+      <label className='flex w-full pl-3 border-b md:border-0' htmlFor='search'>
+        <div className=' flex justify-center items-center'>
+          <RxMagnifyingGlass size={24} opacity='0.7' />
         </div>
-        <ul className='absolute [peer:nth-child(even)>input]:focus:bg-red-400  w-full z-10 top-[90px] border rounded-lg bg-white overflow-hidden'>
+        <input
+          id='search'
+          ref={searchRef}
+          className=' peer w-full p-3 lg:p-4 roundeh-lg '
+          type='text'
+        />
+        <ul
+          className={` peer-focus:block hidden  absolute w-full z-10 top-[60px] left-0 lg:top-[70px] border rounded-lg bg-white overflow-hidden`}
+        >
           <li className='p-1'>결과없음</li>
           <li className='p-1 hover:bg-neutral-100 bg-neutral-50'>결과</li>
         </ul>
-      </div>
+        <button
+          className='w-24 bg-[#1976D2] text-white whitespace-nowrap md:rounded-r-3xl'
+          type='button'
+        >
+          검색
+        </button>
+      </label>
     </div>
   );
 }
