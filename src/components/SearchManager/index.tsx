7662bfe7f4d23c5738f8555a398d9d2ba1b18213
dukjjang <jhd7292@naver.com>
2023-01-10 22:21:42 +0900

    design: input css

--- src/components/SearchManager/index.tsx
+++ src/components/SearchManager/index.tsx
@@ -2,15 +2,13 @@ import { RxMagnifyingGlass } from 'react-icons/rx';
 
 export default function Search() {
   return (
-    <div className={` w-full flex justify-center items-center rounded-lg `}>
-      <div className='relative w-[200px]'>
-        <label className='inline-block' htmlFor='search'>
+    <div className={` w-full flex justify-center items-center `}>
+      <div className='flex justify-center items-center relative border rounded-lg w-[200px]'>
+        <div className=''>
           <RxMagnifyingGlass size={20} />
-          <input
-            id='search'
-            className='w-full p-2 rounded-lg border'
-            type='text'
-          />
+        </div>
+        <label className='' htmlFor='search'>
+          <input id='search' className='w-full p-2 rounded-lg ' type='text' />
         </label>
         <ul className='absolute w-full z-10 top-[45px] border rounded-lg bg-white'>
           <li className='p-1'>결과없음</li>
